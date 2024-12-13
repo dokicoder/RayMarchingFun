dcf5011a11b24090f09cb7e6814a79115e1a0c60
Kay Illner <kay.illner@gmail.com>
2024-12-13 12:16:49 +0100

    re-setup scene (no lighting yet)

--- src/components/MetaballsScene.tsx
+++ src/components/MetaballsScene.tsx
@@ -14,26 +14,96 @@ void main() {
 `;
 
 const fragmentShader = `
-uniform float iTime;
 uniform float aspect;
 uniform float metaBallBlendValue;
 uniform float cameraRotationOffset;
 
 varying vec2 _uv;
 
-//const int MAX_STEPS = 64;
-//const float EPSILON = .0001;
-//const float STEP_SIZE = .975;
-//const float PI = 3.14159265359;
+const int MAX_STEPS = 64;
+// allowed distance from surface
+const float EPSILON = .0001;
+const float STEP_SIZE = .999; // TODO: why not use 1 here?
+const float PI = 3.14159265359;
+const float OUT_BOUNDS_DISTANCE = 1000.0;
 
-void main () {
-  gl_FragColor = vec4 (_uv, 0, 1.);
+float sphereSdf(in vec3 p, in float r) {
+    return length(p) - r;
+}
+
+float opCombine(in float d1, in float d2, in float r) {
+    float h = clamp(.5 + .5 *(d2 - d1) / r, .0, 1.);
+    return mix(d2, d1, h) - r * h *(1. - h);
+}
+
+float metaBallsScene(in vec3 p) {
+    // TODO: from uniforms
+    float r1 = 0.2;
+    float r2 = 0.3;
+
+    vec3 spherePos1 = vec3(0.);
+    vec3 spherePos2 = vec3(-1., 0., 0.);
+
+    float ball1 = sphereSdf(p + spherePos1, r1);
+    float ball2 = sphereSdf(p + spherePos2, r2);
+  
+    float metaBalls = opCombine(ball1, ball2, metaBallBlendValue);
+
+    return metaBalls;
+}
+
+float rayMarch(in vec3 ro, in vec3 rd) {
+  float t = .0;
+  float d = .0;
+  for(int i = 0; i < MAX_STEPS; ++i) {
+      vec3 p = ro + d * rd;
+      t = metaBallsScene(p);
+      if(t < EPSILON) {
+        return d;
+      }
+      d += t*STEP_SIZE;
+  }
+  return OUT_BOUNDS_DISTANCE;
+}
+
+// get camera ray for uv position
+vec3 cameraRay(in vec2 uv, in vec3 rayOrigin, in vec3 cameraTarget, in float zoom) {
+    vec3 camForward = normalize(vec3(cameraTarget - rayOrigin));
+    vec3 worldUp = vec3(.0, 1., .0);
+    vec3 camRight = normalize(cross(worldUp, camForward));
+    vec3 camUp = normalize(cross(camForward, camRight));
+    vec3 camCenter = rayOrigin + camForward * zoom;
+    
+    return normalize(camCenter + uv.x * aspect * camRight + uv.y * camUp - rayOrigin);
+}
+
+void main() {
+  // uv with aspect
+  vec2 uv = (2.0 * _uv - vec2(1., 1.));
+
+  float cameraAngleY = radians(cameraRotationOffset);
+  float cameraDistanceFromTarget = 2.25;
+
+  vec3 cameraOrigin = vec3(cameraDistanceFromTarget * cos(cameraAngleY), 0.0, cameraDistanceFromTarget * -sin(cameraAngleY));
+  
+  float zoom = 1.3;
+  vec3 cameraTarget = vec3(.0);
+
+  
+  vec3 currentRayDirection = cameraRay(uv, cameraOrigin, cameraTarget, zoom);
+  float d = rayMarch (cameraOrigin, currentRayDirection);
+
+  if(d >= OUT_BOUNDS_DISTANCE) {
+    gl_FragColor = vec4(0., 0., 0., 1.);
+  } else {
+    gl_FragColor = vec4(_uv, 0., 1.);
+  }
+
 }`;
 
-// this is the state interface for the component (as the uniforms are the sole thing that is updated)
+// this is the state interface for the component(as the uniforms are the sole thing that is updated)
 interface MetaballUniforms {
   aspect: IUniform;
-  iTime: IUniform;
   metaBallBlendValue: IUniform;
   cameraRotationOffset: IUniform;
 }
@@ -46,10 +116,7 @@ let frameId: number = undefined;
 
 let aspect = 1;
 
-const clock = new Clock();
-
 const uniforms: MetaballUniforms = {
-  iTime: { value: clock.elapsedTime },
   aspect: { value: aspect },
   metaBallBlendValue: { value: 0.5 },
   cameraRotationOffset: { value: 0 },
@@ -112,8 +179,8 @@ const MetaballScene: React.FC = () => {
   };
 
   const reducer = (state: MetaballUniforms, { type, value }: Action) => {
-    // we need to update the uniforms object as well as the state copy to keep animate (bot in react state)
-    // and render (in react state) in sync
+    // we need to update the uniforms object as well as the state copy to keep animate(bot in react state)
+    // and render(in react state) in sync
     // if we try to save everything in react store, THREE.js will still display the initial state because animate() operates on the initial instance of the state object
     uniforms[type].value = value;
     return { ...state, [type]: { value } };
@@ -124,10 +191,6 @@ const MetaballScene: React.FC = () => {
   useEffect(() => {
     const { clientWidth: width, clientHeight: height } = mount;
 
-    clock.start();
-
-    console.log('clock started');
-
     uniforms.aspect.value = width / height;
 
     if (mount.children.length) {
@@ -137,6 +200,7 @@ const MetaballScene: React.FC = () => {
     // add scene
     scene = new Scene();
 
+    // TODO: not needed, or at least make orthographic
     camera = new THREE.PerspectiveCamera(75, uniforms.aspect.value, 0.1, 1000);
 
     // add renderer
