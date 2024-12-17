cba0b49fd64bee32973591c6c74daddb82aa2b24
Kay Illner <kay.illner@gmail.com>
2024-12-17 11:16:10 +0100

    chore: add simulation

--- src/components/MetaballsScene.tsx
+++ src/components/MetaballsScene.tsx
@@ -13,13 +13,272 @@ void main() {
 }
 `;
 
+// the code for verlet simulation here
+
+const NUM_VERLET_ITERATIONS = 3;
+
+const config = {
+  repellentForce: 0.000001,
+  stickCorrectionForce: 0.001,
+  wallBounceDamping: 0.78,
+  gravity: 0.00003,
+  friction: 0.01,
+  borderOffset: 0.0001,
+  targetPullForce: 0,
+} as const;
+
+let { repellentForce, stickCorrectionForce, wallBounceDamping, gravity, friction, borderOffset, targetPullForce } =
+  config;
+
+type Point = {
+  x: number;
+  y: number;
+  prevX: number;
+  prevY: number;
+  fixed?: boolean;
+  isCenter?: boolean;
+};
+
+type Stick = {
+  p0: Point;
+  p1: Point;
+  length: number;
+};
+
+let points: Point[] = [];
+let sticks: Stick[] = [];
+
+// ui state flags
+let simulationMode = 'init';
+let autoChainMode = true;
+
+let useGravity = false;
+
+const lerp = (x: number, y: number, t: number) => (1 - t) * x + t * y;
+
+function length({ x, y }: Point) {
+  return Math.sqrt(x * x + y * y);
+}
+
+function distance(p0: Point, p1: Point) {
+  const dX = p1.x - p0.x;
+  const dY = p1.y - p0.y;
+
+  return Math.sqrt(dX * dX + dY * dY);
+}
+
+// applies physics to the points
+function updatePoints() {
+  let pairCount = 0;
+
+  points.forEach((p, idx) => {
+    if (p.fixed) {
+      return;
+    }
+
+    let vX = p.x - p.prevX;
+    let vY = p.y - p.prevY;
+
+    // we check points pairwise, but also, we check each pair twice, as Pa, Pb and as Pb, Pa
+    // this is correct because this way each end of the pair gets updated
+    // just make sure to do the stuff that is referring to the unordered pair only once
+    // for (let otherIdx = idx + 1; otherIdx < points.length; ++otherIdx) {
+    //   otherP = points[otherIdx];
+    points.forEach((otherP, otherIdx) => {
+      // do not relate points with themselves
+      if (idx === otherIdx) {
+        return;
+      }
+
+      ++pairCount;
+
+      if (p.isCenter || otherP.isCenter) {
+        return;
+      }
+
+      const dist = distance(p, otherP);
+
+      // heuristic for same point, could be 0, but we choose our parameters so that initially points do not coincide
+      // if (dist < 1 || isNaN(dist)) {
+      //   return;
+      // }
+
+      const repellentVector = repellentForce / (dist * dist);
+
+      const pullVecX = ((p.x - otherP.x) / dist) * repellentVector;
+      const pullVecY = ((p.y - otherP.y) / dist) * repellentVector;
+
+      vX += pullVecX;
+      vY += pullVecY;
+    });
+
+    vX *= 1 - friction;
+    vY *= 1 - friction;
+
+    p.prevX = p.x;
+    p.prevY = p.y;
+
+    // TODO: time step dependence
+    p.x += vX;
+    p.y += vY;
+
+    // apply gravity
+    // TODO: convert between coordinate systems so we can subtract gravity and this becomes less confusing
+    // MIND: gravity is an accelerating force, so it should alter velocity to be strict
+    // this here will have the same end result since the change in position will result in reduced gravity during next update
+
+    if (useGravity) {
+      p.y += gravity;
+    }
+  });
+
+  console.log('pairCount', pairCount);
+}
+
+// constraints point position with the sticks
+function updateSticks() {
+  sticks.forEach(s => {
+    const dX = s.p1.x - s.p0.x;
+    const dY = s.p1.y - s.p0.y;
+
+    const currentLength = distance(s.p0, s.p1);
+    const dLength = currentLength - s.length;
+
+    const relativeLengthChange = dLength / currentLength;
+
+    const offsetX = dX * stickCorrectionForce * relativeLengthChange;
+    const offsetY = dY * stickCorrectionForce * relativeLengthChange;
+
+    // this is a strictly speaking more correct implementation,
+    // but the code below works just as well due to the iterations
+    /*
+    if (s.p0.fixed && s.p1.fixed) {
+      return;
+    }
+    // if either of the points is fixed,
+    // the other one has to compensate for the whole dLength, so we double the offsets again
+    else if (s.p0.fixed) {
+      s.p1.x += 2 * offsetX;
+      s.p1.y += 2 * offsetY;
+
+      return;
+    } else if (s.p1.fixed) {
+      s.p0.x -= 2 * offsetX;
+      s.p0.y -= 2 * offsetY;
+
+      return;
+    }
+    */
+
+    if (!s.p0.fixed) {
+      s.p0.x += offsetX;
+      s.p0.y += offsetY;
+    }
+    if (!s.p1.fixed) {
+      s.p1.x -= offsetX;
+      s.p1.y -= offsetY;
+    }
+  });
+}
+
+function constrainBorders() {
+  points.forEach(p => {
+    if (p.fixed) {
+      return;
+    }
+
+    let vX = p.x - p.prevX;
+    let vY = p.y - p.prevY;
+
+    // bounce at borders
+    if (p.x > 0.5 - borderOffset) {
+      p.x = 0.5 - borderOffset;
+      p.prevX = p.x + vX * wallBounceDamping;
+    }
+    if (p.x < -0.5 + borderOffset) {
+      p.x = -0.5 + borderOffset;
+      p.prevX = p.x + vX * wallBounceDamping;
+    }
+    if (p.y > 0.5 - borderOffset) {
+      p.y = 0.5 - borderOffset;
+      p.prevY = p.y + vY * wallBounceDamping;
+    }
+    if (p.y < -0.5 + borderOffset) {
+      p.y = -0.5 + borderOffset;
+      p.prevY = p.y + vY * wallBounceDamping;
+    }
+  });
+}
+
+function createRingPoint(angle: number, radius: number) {
+  const x = Math.cos(angle) * radius - 0.05 + Math.random() * 0.1;
+  const y = Math.sin(angle) * radius - 0.05 + Math.random() * 0.1;
+
+  //const speedInferScale = 0.6;
+  // const prevX = Math.cos(angle) * speedInferScale * radius + 600;
+  // const prevY = Math.sin(angle) * speedInferScale * radius + 600;
+
+  return {
+    x,
+    y,
+    prevX: x,
+    prevY: y,
+  };
+}
+
+function initSimulation() {
+  points = [];
+  sticks = [];
+
+  const centerPoint = {
+    x: 0,
+    y: 0,
+    prevX: 0,
+    prevY: 0,
+    isCenter: true,
+  };
+
+  points.push(centerPoint);
+
+  for (let i = 0; i < 10; ++i) {
+    const newPoint = createRingPoint((Math.PI / 5) * i, 0.3);
+
+    points.push(newPoint);
+
+    sticks.push({
+      p0: centerPoint,
+      p1: newPoint,
+      length: 0.3,
+    });
+  }
+}
+
+function toggleSimulation() {
+  if (simulationMode === 'init' || simulationMode === 'paused') {
+    simulationMode = 'running';
+  } else {
+    simulationMode = 'paused';
+  }
+}
+
+function resetSimulation() {
+  initSimulation();
+  simulationMode = 'init';
+}
+
+function toggleGravity() {
+  useGravity = !useGravity;
+}
+
+//
+
 const fragmentShader = `
 #define PI 3.1415926538
 
 uniform float aspect;
 uniform float metaBallBlendValue;
 uniform float cameraRotationOffset;
-uniform vec3 metaBallPositions[ 10 ];
+uniform vec3 metaBallPositions[ 11 ];
 
 in vec2 _uv;
 
@@ -41,15 +300,15 @@ float opCombine(in float d1, in float d2, in float r) {
 
 float scene(in vec3 p) {
     // TODO: from uniforms
-    float rCenter = 0.9;
+    float rCenter = 0.3;
     float rOther = 0.9;
 
     vec3 spherePosCenter = vec3(0.);
-    float ballCenter = sphereSdf(p + spherePosCenter, rCenter);
+    float ballCenter = sphereSdf(p + metaBallPositions[0], rCenter);
 
     float metaBalls = ballCenter;
     
-    for(int i=0;i<10;++i)
+    for(int i=1;i<11;++i)
     {
       float angle = float(i) * 0.2 * PI;
       //vec3 spherePosOuter = vec3(cos(angle), -sin(angle), 0.0) * 3.4;
@@ -240,11 +499,10 @@ const uniforms: MetaballUniforms = {
   metaBallBlendValue: { value: 0.5 },
   cameraRotationOffset: { value: 90 },
   metaBallPositions: {
-    value: [...Array(10).keys()].map(idx => {
-      const angle = idx * 0.2 * Math.PI;
-
-      return new THREE.Vector3(Math.cos(angle), -Math.sin(angle), 0.0).multiplyScalar(3.4);
-    }),
+    // value: [...Array(10).keys()].map(idx => {
+    //   const angle = idx * 0.2 * Math.PI;
+    //   return new THREE.Vector3(Math.cos(angle), -Math.sin(angle), 0.0).multiplyScalar(3.4);
+    // }),
   },
 };
 
@@ -288,6 +546,19 @@ const MetaballScene: React.FC = () => {
     Object.entries(uniforms).forEach(([key, { value }]) => {
       material.uniforms[key].value = value;
     });
+
+    if (simulationMode === 'running') {
+      updatePoints();
+
+      uniforms.metaBallPositions.value = points.map(p => {
+        return new THREE.Vector3(p.x, p.y, 0.0).multiplyScalar(10);
+      });
+
+      for (let i = 0; i < NUM_VERLET_ITERATIONS; ++i) {
+        updateSticks();
+        constrainBorders();
+      }
+    }
 
     renderScene();
     frameId = requestAnimationFrame(animate);
@@ -336,6 +607,9 @@ const MetaballScene: React.FC = () => {
     mount.appendChild(renderer.domElement);
 
     scene.add(Plane());
+
+    initSimulation();
+    toggleSimulation();
 
     start();
 
