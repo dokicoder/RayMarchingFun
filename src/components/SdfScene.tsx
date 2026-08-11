df33942781d9bda1e188c147c1bf75477463e778
Kay Illner <kay.illner@gmail.com>
2026-08-11 12:26:23 +0200

    Look, ma: chad-cn buttons stolen from the internet

--- src/components/SdfScene.tsx
+++ src/components/SdfScene.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useReducer, useRef } from 'react';
 import * as THREE from 'three';
-import { Scene, WebGLRenderer, Camera, Clock, IUniform } from 'three';
+import { Scene, WebGLRenderer, Camera } from 'three';
+import type { IUniform } from 'three';
 import { Slider } from './ui/slider';
 import { Switch } from './ui/switch';
+import { Label } from './ui/label';
 
 // jut for
 const vert = (x) => x.toString();
@@ -454,20 +456,27 @@ export const SdfScene: React.FC = () => {
       <div style={{ width: '1300px', height: '800px' }} ref={canvasContainerRef} />
       {/* TODO: debounce */}
       <div style={{ padding: 12, border: "3px dashed red", color: "white" }}>
-        <Slider
-          className="mx-auto w-full max-w-xs p-4"
 
-          id="cameraRotation"
-          value={stateUniforms.cameraRotationOffset.value}
-          min={0}
-          max={360}
+        <div className="flex items-center gap-2">
+          <Label htmlFor="dark-mode">Rotation</Label>
+          <Slider
+            className="max-w-xs"
 
-          onValueChange={value => {
-            dispatch({ type: 'cameraRotationOffset', value });
-          }}
-        />
-        Switch
-        <Switch />
+            id="cameraRotation"
+            value={stateUniforms.cameraRotationOffset.value}
+            min={0}
+            max={360}
+
+            onValueChange={value => {
+              dispatch({ type: 'cameraRotationOffset', value });
+            }}
+          />
+        </div>
+        <div className="flex items-center gap-2">
+          <Label htmlFor="dark-mode">Dark Mode</Label>
+          <Switch id="dark-mode" />
+        </div>
+
 
       </div>
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import './style.css';
 import { StrictMode } from "react";
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
+import { ThemeProvider } from "@/src/components/theme-provider"
 
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
